49ea09ac44b617fa144acc4a443415059d3ecc4c
Pan, Wen-Ming <smalljelly2018@gmail.com>
2022-12-14 16:15:03 +0800

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -11,8 +11,10 @@ gcloud iam service-accounts delete gcs-vvp-service-acc@speedy-victory-336109.iam
 echo "delete bucket"
 gcloud storage buckets delete gs://$bucket --project=$project_id --quiet
 
-echo "delete gke cluster"
+echo "deleting gke cluster"
 gcloud container clusters delete $name \
     --zone $zone \
     --project=$project_id \
     --quiet
+    
+ echo "destroy done"
